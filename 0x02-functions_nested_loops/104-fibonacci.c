ac4d27fa85cb91bc29cafdeba1479c945f041525
everobi <everobiofficial@gmail.com>
2023-07-19 14:14:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_times_table - Prints the first 98 fibonacci numbers, starting with
+ * main - Prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, separated by a comma followed by a space
  * Return: Always 0
  */
@@ -11,7 +11,7 @@ int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
 unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
-for (count = 0; count <92; count++)
+for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
 printf("%lu, ", sum);
@@ -21,12 +21,12 @@ fib2 = sum;
 fib1_half1 = fib1 / 10000000000;
 fib2_half1 = fib2 / 10000000000;
 fib1_half2 = fib1 % 10000000000;
-fib2_half2 = fib2% 10000000000;
+fib2_half2 = fib2 % 10000000000;
 for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
-if (fib1_half2 + fib2_half2> 9999999999)
+if (fib1_half2 + fib2_half2 > 9999999999)
 {
 half1 += 1;
 half2 = 10000000000;
