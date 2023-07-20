d60ad7da7490fc992e1d3310057d3ba7221efb10
everobi <everobiofficial@gmail.com>
2023-07-20 08:09:28 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,6 +3,7 @@
  * nul - Multiplies two integers
  * @x: The first number to multiply
  * @y: The second number to multiply with the first number
+ * Return: The result of multiplication between x param and y param
  */
 int nul(int x, int y)
 {
