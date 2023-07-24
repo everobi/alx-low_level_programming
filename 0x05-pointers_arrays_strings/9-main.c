a2d928b3119ddb4e314ebf8a1e761e29ca23d361
everobi <everobiofficial@gmail.com>
2023-07-24 16:12:29 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+char *_strcpy(char *dest, char *src);
 int main(void)
 {
 char s1[98];
