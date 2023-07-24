df0e667589fea23325ff22b58cf10757113ffae7
everobi <everobiofficial@gmail.com>
2023-07-24 12:47:33 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,13 @@ void puts2(char *str)
 {
 int i;
 int j = 0;
-while (str[j] != '\0')
+while (*str[j] != '\0')
 {
 j++;
 }
 for (i = 0; i <j; i += 2)
 {
 char *str;
-puts2(str[i]);
+puts2(*str[i]);
 }
-('\n');
 }
