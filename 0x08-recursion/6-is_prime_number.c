1807195fb2177eb6a4b4bd3c733457a33f73df9c
everobi <everobiofficial@gmail.com>
2023-08-03 05:56:49 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,8 +14,9 @@ return (check_prime(n, 1));
 /**
 * check_prime - Check if number is prime *@n: the number to be checked
 * @i: the iteration times
+* @n: the number to be checked
 *
-* Return: 1 for prime or 0 composite 
+* Return: 1 for prime or 0 composite
 */
 int check_prime(int n, int i)
 {
