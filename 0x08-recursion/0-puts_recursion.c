151a3104847f3c8230e0921d344e0ec16e9fbaad
everobi <everobiofficial@gmail.com>
2023-08-03 03:38:27 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,15 @@
  * @s: the string to print
  * Return: Nothing.
  */
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
 _putchar('\n');
 return;
 }
-_putchar(*s);
 s++;
+_putchar('\n');
+return;
+_putchar(*<s);
 _puts_recursion(s);
-}
