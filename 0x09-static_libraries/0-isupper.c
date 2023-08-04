37621e80282258551b72d23bd5e804939b3fc6d0
everobi <everobiofficial@gmail.com>
2023-08-04 12:03:19 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * _isupper - check if uppercase letters
+ * @c: char to check
+ * return: if uppercase return 1, else return 0
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
