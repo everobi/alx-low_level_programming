0ed3fd8d8573beccf6c4cb4c0aadc9f619c5745e
everobi <everobiofficial@gmail.com>
2023-08-08 11:42:14 -0700

    2-strchr.o

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _isupper - check if uppercase letters
  * @c: char to check
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * memcpy - a function that copies memory area
+ * @dest: memory where is stored
+ * @sre: memory where is copied
+ * @n: number of bytes
+ * Return: copied memory with n byted changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int p = 0;
+int i = n;
+for (; p < i; p++)
+{
+dest[p] = src[p];
+n--;
+}
+return (dest);
+}
